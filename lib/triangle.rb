d903b3f30467ebafdd2d1a3ea6cdd8a676d72bac
nicolewang2000 <55036091+nicolewang2000@users.noreply.github.com>
2020-05-30 19:26:51 -0400

    Update triangle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,11 +23,3 @@ class Triangle
   class TriangleError < StandardError
   end
 end
-
-# def validate_triangle
-#   real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
-#   [a, b, c].each do |side|
-#     real_triangle << false if side <= 0 
-#   raise TriangleError if real_triangle.include?(false)
-#   end
-# end
